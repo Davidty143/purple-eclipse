ae4524d188054b1fc26344dc2f37fd7ca7c960fe
davidty143 <22-1-00262@vsu.edu.ph>
2025-04-19 12:31:14 +0800

    Add Footer layout footer

--- app/(landingPage)/page.tsx
+++ app/(landingPage)/page.tsx
@@ -1,28 +1,27 @@
-//LandingPage
 import { NewTopics } from './components/NewTopics';
 import { PopularSubforumsGrid } from './components/PopularSubforums';
 import BodyHeader from '@/components/BodyHeader';
+import Footer from '@/components/Footer';
+
 const LandingPage = () => {
   return (
-    <div className="min-h-screen flex justify-center py-8">
-      {/* Focus Content */}
-      <div className="w-[1250px] 2xl:w-[80%] flex flex-col px-3">
-        {/* Main Body */}
-        <div className="w-full flex flex-col lg:flex-row justify-between gap-8">
-          {/* Content Body */}
-          <div className="w-full flex flex-col gap-4">
-            <div className="border-b-2 mb-3">
-              <BodyHeader />
+    <div className="min-h-screen flex flex-col justify-between">
+      <div className="flex justify-center py-8 flex-grow">
+        <div className="w-[1250px] 2xl:w-[80%] flex flex-col px-3">
+          <div className="w-full flex flex-col lg:flex-row justify-between gap-8">
+            <div className="w-full flex flex-col gap-4">
+              <div className="border-b-2 mb-3">
+                <BodyHeader />
+              </div>
+              <PopularSubforumsGrid />
             </div>
-
-            <PopularSubforumsGrid />
-          </div>
-          <div className="flex flex-1 flex-col space-y-14 max-h-auto h-auto">
-            <NewTopics />
+            <div className="flex flex-1 flex-col space-y-14 max-h-auto h-auto">
+              <NewTopics />
+            </div>
           </div>
         </div>
-        <div className="w-full h-[10vh]">FOOTER</div>
       </div>
+      <Footer />
     </div>
   );
 };
