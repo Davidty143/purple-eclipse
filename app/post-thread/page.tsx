8b48d148347a5dbcaa7cbd1154083c1ef6f18891
davidty143 <22-1-00262@vsu.edu.ph>
2025-05-03 20:33:37 +0800

    Refactor add thread page
    Improve general UI and make the components consistent across the platform, including the select subforum dialog, category dropdown, and the title text box.
    
    Add icons and change the general color themes of the components.
    
    Make it responsive for smaller screen sizes.

--- app/post-thread/page.tsx
+++ app/post-thread/page.tsx
@@ -1,8 +1,20 @@
-// app/post-thread/page.tsx
 import PostThread from './components/PostThread';
+import { NewTopics } from '@/app/(landingPage)/components/NewTopics'; // If you need this sidebar content
+import BodyHeader from '@/components/BodyHeader'; // You can include this or customize a header for your page
 
-const Page = () => {
-  return <PostThread />;
+const PostThreadPage = () => {
+  return (
+    <div className="min-h-screen flex justify-center py-8">
+      <div className="w-[1250px] 2xl:w-[80%] flex flex-col px-3">
+        <div className="w-full flex flex-col lg:flex-row justify-between gap-8">
+          <div className="w-full flex flex-col gap-6">
+            <PostThread />
+          </div>
+        </div>
+        <footer className="w-full mt-auto py-4"></footer>
+      </div>
+    </div>
+  );
 };
 
-export default Page;
+export default PostThreadPage;
