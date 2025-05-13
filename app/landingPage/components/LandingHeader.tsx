a6d785e3a01382fa9bc8f63d97e2558aaf4c9da2
davidty143 <22-1-00262@vsu.edu.ph>
2025-05-13 12:18:47 +0800

    Remove redundant landing page file

--- app/landingPage/components/LandingHeader.tsx
+++
@@ -1,9 +0,0 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import BodyHeader from '@/components/BodyHeader';
-export function LandingHeader() {
-  return (
-    <Card className="w-auto h-32 bg-white rounded-[10px]">
-      <BodyHeader />
-    </Card>
-  );
-}
