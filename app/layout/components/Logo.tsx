8ced6ca6b74cbdc0f5b05f0ca763482e83a2547f
davidty143 <22-1-00262@vsu.edu.ph>
2025-05-18 03:52:42 +0800

    Fix logo in the header
    Apply layout styles immediately (inline) and remove next/image.

--- app/layout/components/Logo.tsx
+++ app/layout/components/Logo.tsx
@@ -1,10 +1,21 @@
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default function Logo() {
   return (
     <Link href="/" className="block">
-      <Image src="/visconn_transaprent6.png" alt="Visconn Logo" width={160} height={60} priority loading="eager" className="object-contain" />
+      <img
+        src="/visconn_transaprent6.png"
+        alt="Visconn Logo"
+        width={160}
+        height={60}
+        className="block"
+        style={{
+          display: 'block',
+          width: '160px',
+          height: '60px',
+          objectFit: 'contain'
+        }}
+      />
     </Link>
   );
 }
