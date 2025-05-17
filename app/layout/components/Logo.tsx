20c8fdd1f84aeeac9c52a3cc263a8e69917045ce
davidty143 <22-1-00262@vsu.edu.ph>
2025-05-18 03:26:18 +0800

    Fix logo in the header
    Make the width size of the logo the same as the header layout component and fixed.

--- app/layout/components/Logo.tsx
+++ app/layout/components/Logo.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 
 export default function Logo() {
   return (
-    <Link href="/">
-      <div className="h-[60px] w-[160px] flex items-center justify-start">
+    <Link href="/" className="block">
+      <div className="h-[60px] w-[160px] relative flex items-center justify-start">
         <Image src="/visconn_transaprent6.png" alt="Visconn Logo" width={160} height={60} priority loading="eager" className="object-contain" />
       </div>
     </Link>
