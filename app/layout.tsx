c1b0262516133db8ac7506c98c11da8a7d4d072d
davidty143 <22-1-00262@vsu.edu.ph>
2025-05-18 04:44:31 +0800

    Fix logo in the header
    Add the photo's actual size to its aspect ratio.

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,16 +52,7 @@ export default async function RootLayout({
                   <div className="ml-2 flex items-center justify-start gap-2 min-h-[60px]">
                     <MobileSidebarMenu />
                     <div className="w-[160px] h-[60px] relative overflow-hidden">
-                      <img
-                        src="/visconn_transaprent6.png"
-                        alt="Visconn Logo"
-                        width={160}
-                        height={60}
-                        className="object-contain w-full h-full block"
-                        style={{
-                          aspectRatio: '160 / 60'
-                        }}
-                      />
+                      <img src="/visconn_transaprent6.png" alt="Visconn Logo" width={160} height={60} className="object-contain w-full h-full block" style={{ aspectRatio: '414 / 103' }} />
                     </div>
                   </div>
 
