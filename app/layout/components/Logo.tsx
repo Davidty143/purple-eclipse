ec5e26e7b055e5775b02178b4b3b8f9d9367a6f6
davidty143 <22-1-00262@vsu.edu.ph>
2025-05-18 03:38:02 +0800

    Fix logo header
    Remove height padding in the parent container, which is the header in the layout.

--- app/layout/components/Logo.tsx
+++ app/layout/components/Logo.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <Link href="/" className="block">
       <div className="relative" style={{ width: '160px', height: '60px' }}>
-        <Image src="/visconn_transaprent6.png" alt="Visconn Logo" fill priority loading="eager" className="object-contain" />
+        <Image src="/visconn_transaprent6.png" alt="Visconn Logo" fill priority loading="eager" sizes="160px" className="object-contain" />
       </div>
     </Link>
   );
